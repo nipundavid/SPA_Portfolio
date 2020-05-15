e6cf3f8f46a0cd6fc64006181b78a703a33894f7
Nipun David <nipun19david@yahoo.com>
2020-05-16 03:09:38 +0530

    footer added, npm styled-componet added in footer.js

--- spa-portfolio/src/App.js
+++ spa-portfolio/src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <h2>
       <NavBar />
+      <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facere
+      praesentium odio quibusdam, quos dolorem nobis quia eveniet quas libero
+      quod consectetur eligendi nesciunt eius nihil earum iusto maiores! Sequi?
+      <br />
+      <Footer />
     </h2>
   );
 }
